bdbbb84d0e07730685ee4e8939d8e7dfdffb527c
Ali Gardezi <sgardezi@google.com>
2025-08-28 08:53:21 +0100

    updates

--- reg-deepresearch-agent/agent.py
+++ reg-deepresearch-agent/agent.py
@@ -1,17 +1,3 @@
-# Copyright 2025 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import datetime
 import logging
 import re
@@ -229,7 +215,7 @@ class PRAEscalationChecker(BaseAgent):
 
 # --- AGENT DEFINITIONS ---
 plan_generator = LlmAgent(
-    model=config.worker_model,
+    model=config.plan_model,
     name="plan_generator",
     description="Generates or refine the existing 5 line action-oriented research plan, using minimal search only for topic clarification.",
     instruction=f"""
